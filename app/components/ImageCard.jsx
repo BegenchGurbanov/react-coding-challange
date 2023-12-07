66eaddab959298a147489b8281c350be233cee98
Begench Gurbanov <gurbanovbegenc91@gmail.com>
2023-12-07 18:40:53 -0500

    Feature: add ImageCard component

--- app/components/ImageCard.jsx
+++ app/components/ImageCard.jsx
@@ -1,23 +1,43 @@
 "use client"; // use client-side rendering
 
 import Image from "next/image";
+import { useState } from "react";
+import ImageModal from "./ImageModal";
 
-function ImageCard() {
+function ImageCard({ imgUrl, altDescription }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <Image
-        src={
-          "https://unsplash.com/photos/a-snow-covered-road-surrounded-by-tall-pine-trees-y15WUAnF2Mk"
-        }
-        alt={"Placeholder"}
-        width={800}
-        height={600}
-        objectFit="contain"
-        objectPosition="center"
-        className="rounded"
-      />
-    </div>
+    <>
+      <li style={{ width: "100%", height: "500px" }} onClick={openModal}>
+        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+          <Image
+            src={imgUrl}
+            alt={altDescription}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            className="rounded"
+            loading="lazy"
+          />
+        </div>
+      </li>
+      {isModalOpen && (
+        <ImageModal
+          imgUrl={imgUrl}
+          altDescription={altDescription}
+          closeModal={closeModal}
+        />
+      )}
+    </>
   );
 }
-
 export default ImageCard;
