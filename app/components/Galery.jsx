d46d1e074242da1aed9e25da3c6e4d46b03d1307
Begench Gurbanov <gurbanovbegenc91@gmail.com>
2023-12-07 23:07:24 -0500

    Feature: last lazy load update

--- app/components/Galery.jsx
+++ app/components/Galery.jsx
@@ -6,7 +6,7 @@ import ImageCard from "./ImageCard";
 function Galery() {
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  console.log("images>>>>>", images);
+  //console.log("images>>>>>", images);
   //   fetch images from the API and set the images state
   useEffect(() => {
     const fetchData = async () => {
@@ -26,16 +26,19 @@ function Galery() {
     // when user scrolls to the bottom of the page, fetch more images
 
     const handleScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        console.log(
-          "you're at the bottom of the page new data will be fetched"
-        );
-        setCurrentPage(currentPage + 1);
-        // Show loading spinner and make fetch request to api
-      }
+      console.log("you're at the bottom of the page new data will be fetched");
+      //   if (
+      //     window.innerHeight + document.documentElement.scrollTop ===
+      //     document.documentElement.offsetHeight
+      //   ) {
+      const hasReachedBottom =
+        document.documentElement.offsetHeight -
+          (window.innerHeight + document.documentElement.scrollTop) <=
+        10;
+      setCurrentPage(currentPage + 1);
+      //setCurrentPage(currentPage + 1);
+      // Show loading spinner and make fetch request to api
+      //}
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -48,7 +51,7 @@ function Galery() {
   // STYLES :https://tailwindcss.com/docs/grid-template-columns
   return (
     <ul
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 p-4 "
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4 p-4 "
       style={{ height: "100%" }}
     >
       {/* for each image create an Image component */}
