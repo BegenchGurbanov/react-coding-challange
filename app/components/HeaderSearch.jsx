44dc3219b810a64783c720483f62dbe3fed337a3
Begench Gurbanov <gurbanovbegenc91@gmail.com>
2023-12-16 23:54:52 -0500

    Feature: Add ImageHeader file

---
+++ app/components/HeaderSearch.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+const HeaderSearch = () => {
+  return (
+    <div className="bg-gray-900 flex items-center py-10">
+      <div className="max-w-md mx-auto w-full">
+        <h1 className="text-white text-center text-4x2 font-bold mb-5">
+          Welcome to Photo Gallery
+        </h1>
+      </div>
+    </div>
+  );
+};
+export default HeaderSearch;
