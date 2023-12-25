2e0e7ad51baef0a04922d2058547aabb2f1c74f5
Begench Gurbanov <gurbanovbegenc91@gmail.com>
2023-12-25 18:42:52 -0500

    Header update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="bg-gray-900 flex items-center py-10">
       <div className="max-w-md mx-auto w-full">
         <h1 className="text-white text-center text-4x2 font-bold mb-5">
-          Welcome to Photo Gallery
+          Welcome to Photo Galery
         </h1>
       </div>
     </div>
